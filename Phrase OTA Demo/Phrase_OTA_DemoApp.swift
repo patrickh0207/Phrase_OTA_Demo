4cb61da46e437fc6a8ae3e91a75b8c893ce7d08e
Patrick Hampel <patrick.hampel@phrase.com>
2021-08-23 14:12:05 +0200

    Initial commit

--- Phrase OTA Demo/Phrase_OTA_DemoApp.swift
+++ Phrase OTA Demo/Phrase_OTA_DemoApp.swift
@@ -6,9 +6,30 @@
 //
 
 import SwiftUI
+import PhraseSDK
 
 @main
 struct Phrase_OTA_DemoApp: App {
+    init() {
+        Phrase.shared.setup(
+          distributionID: "",
+          environmentSecret: ""
+          // timeout: 10000,
+          //localeOverride: "de"
+        )
+        do {
+            try Phrase.shared.updateTranslations { result in
+                switch result {
+                case .success(_):
+                    print("Translations download successful.")
+                case .failure:
+                    print("Translations download failed")
+                }
+            }
+        } catch {
+            print("error")
+        }
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
