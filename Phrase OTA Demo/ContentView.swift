db41ab8dcf7e65cb1582cfa8d019e8f63330e29b
Patrick Hampel <patrick.hampel@phrase.com>
2021-08-23 14:02:09 +0200

    Initial Commit

---
+++ Phrase OTA Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Phrase OTA Demo
+//
+//  Created by Patrick Hampel on 23.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Phrase OTA Demo/Phrase_OTA_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phrase_OTA_DemoApp.swift
+//  Phrase OTA Demo
+//
+//  Created by Patrick Hampel on 23.08.21.
+//
+
+import SwiftUI
+
+@main
+struct Phrase_OTA_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
